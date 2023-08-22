00359fc40481950081f0b859650d2a6911b81c52
Caio Lima <caio.gustavolima7777@gmail.com>
2023-08-22 16:10:04 -0300

    cria funcionalidade de votar

--- polls/urls.py
+++ polls/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path('', IndexView.as_view(),name='index'),
     path('pergunta/<int:pk>', QuestionView.as_view(),name='question'),
+    path("pergunta/<int:pk>/vote/", vote, name="vote"),
     path('pergunta/<int:pk>/results', ResultsView.as_view(),name='results'),
 ]
